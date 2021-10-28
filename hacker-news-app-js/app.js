57db30d58c1f6d1fab2ccd66c2f5dda027c80e6c
jincrates <94jingyu@gmail.com>
2021-10-28 23:47:28 +0900

    페이징 구현하기

--- hacker-news-app-js/app.js
+++ hacker-news-app-js/app.js
@@ -5,7 +5,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //@id로 Marking
-
+const store = {
+    currentPage: 1,
+}
 function getData(url) {
     ajax.open('GET', url, false);
     ajax.send(); //데이터를 가져옴
@@ -18,29 +20,34 @@ function newsFeed() {
     const newsList = [];
 
     newsList.push('<ul>');
-    for (let i = 0; i < 10; i++) {
+    for (let i = store.currentPage - 1; i < store.currentPage * 10; i++) {
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     }
     newsList.push('</ul>');
-
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">Prev</a>
+            <a href="#/page/${store.currentPage + 1}">Next</a>
+        </div>
+    `);
     container.innerHTML = newsList.join('');  //배열 안에 들어있는 요소를 모두 합쳐줌
 }
 
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
 
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `; 
 }
@@ -50,6 +57,9 @@ function rounter() {
 
     //첫 진입일 때
     if (routePath === '') {
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
--- hacker-news-app-js/dist/index.605e7089.js
+++ hacker-news-app-js/dist/index.605e7089.js
@@ -465,6 +465,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //@id로 Marking
+const store = {
+    currentPage: 1
+};
 function getData(url) {
     ajax.open('GET', url, false);
     ajax.send(); //데이터를 가져옴
@@ -474,24 +477,30 @@ function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
     newsList.push('<ul>');
-    for(let i = 0; i < 10; i++)newsList.push(`
+    for(let i = store.currentPage - 1; i < store.currentPage * 10; i++)newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     newsList.push('</ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">Prev</a>
+            <a href="#/page/${store.currentPage + 1}">Next</a>
+        </div>
+    `);
     container.innerHTML = newsList.join(''); //배열 안에 들어있는 요소를 모두 합쳐줌
 }
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -499,7 +508,10 @@ function rounter() {
     const routePath = location.hash;
     //첫 진입일 때
     if (routePath === '') newsFeed();
-    else newsDetail();
+    else if (routePath.indexOf('#/page/') >= 0) {
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed();
+    } else newsDetail();
 }
 window.addEventListener('hashchange', rounter);
 rounter();
