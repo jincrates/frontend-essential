2a1a271cda045db068d4b1321860776684febd5c
jincrates <94jingyu@gmail.com>
2021-10-29 22:05:21 +0900

    typescript 지시어

--- hacker-news-app-ts/app.ts
+++ hacker-news-app-ts/app.ts
@@ -6,27 +6,27 @@ interface Store {
 }
 
 interface News {
-    id: number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string;
+    readonly id: number;  //readonly 변경불가 지시어
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
 }
 
 interface NewsFeed extends News { 
-    comments_count: number;
-    points: number;
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean;  //? : optional
 }
 
 interface NewsDetail extends News {
-    comments: NewsComment[];
+    readonly comments: NewsComment[];
 }
 
 interface NewsComment extends News {
-    comments: NewsComment[];
-    level: number;
+    readonly comments: NewsComment[];
+    readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
